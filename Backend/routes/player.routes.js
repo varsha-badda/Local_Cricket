58e4d731e3aaaccb52e526429ff9ab238141a926
varsha-badda <varshabadda0@gmail.com>
2025-12-25 11:34:07 +0530

    player

--- Backend/routes/player.routes.js
+++ Backend/routes/player.routes.js
@@ -1,1 +1,14 @@
-const express = require("express")
+const express = require("express");
+const router = express.Router();
+const {
+  getAllPlayers,
+  createPlayer
+} = require("../controllers/player.controller");
+
+/* VIEW players */
+router.get("/", getAllPlayers);
+
+/* ADD player */
+router.post("/", createPlayer);
+
+module.exports = router;
